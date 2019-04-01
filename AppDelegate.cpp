83b183ec3084cf39374bf67dc89752c32f6adc08
Arka Mukherjee <Arka161@gmail.com>
2019-04-01 18:44:09 +0530

    Branch Commit

--- AppDelegate.cpp
+++ AppDelegate.cpp
@@ -24,7 +24,10 @@ static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
-
+void none()
+{
+	
+}
 AppDelegate::AppDelegate()
 {
 }
